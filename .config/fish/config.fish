620b56d0ec26586bd59d39bd04db1a4641017680
Dylan Bettendroffer <contact@dylanbettendroffer.fr>
2022-01-09 14:29:20 +0100

    Updates after using xmonad and xmobar on my main machine for a few weeks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,12 +13,13 @@ alias grun='java org.antlr.v4.gui.TestRig'
 alias choco='java -classpath /usr/local/lib/choco-solver-4.10.7-jar-with-dependencies.jar'
 alias ls='exa -1l --icons'
 alias grep='rg'
-alias ranger='joshuto'
+#alias ranger='joshuto'
 alias ps='procs'
 alias tlmgr='/usr/share/texmf-dist/scripts/texlive/tlmgr.pl --usermode'
 
-[ -f "/home/dymdym/.ghcup/env" ] && source "/home/dymdym/.ghcup/env" # ghcup-env
+# [ -f "/home/dymdym/.ghcup/env" ] && source "/home/dymdym/.ghcup/env" # ghcup-env
 
-export BW_SESSION="6z8JLaphvMi5caEQpy1mYKuZWDZbUtzrXJ/P6eW52w/YGN/lRBpkBip9NejtxPDHBkD7LApTKb1tQ/lA7yhQ4A=="
+# export BW_SESSION="6z8JLaphvMi5caEQpy1mYKuZWDZbUtzrXJ/P6eW52w/YGN/lRBpkBip9NejtxPDHBkD7LApTKb1tQ/lA7yhQ4A=="
 
 fish_add_path -p /home/dymdym/.local/bin/ .
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/dymdym/.ghcup/bin # ghcup-env
